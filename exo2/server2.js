6020ec2ee934dbdf1e1a57ec8cc55227c46b14a9
Quang Minh PHAM <quangminh1575@gmail.com>
2023-11-21 09:37:43 +0100

    update

---
+++ exo2/server2.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const port = 4000;
+const directoryServerUrl = "http://localhost:5000";
+
+// Informer le serveur annuaire de son adresse
+axios.post(`${directoryServerUrl}/register`, { serverName: "Server2", address: `http://localhost:${port}` });
+
+// Utilisation de bodyParser pour traiter le corps des requêtes POST
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/", async (req, res) => {
+  if (req.body && req.body.message === "pong") {
+    console.log("pong");
+
+    // Obtenir l'adresse du serveur 1 depuis le serveur annuaire
+    const response = await axios.post(`${directoryServerUrl}/getAddress`, { serverName: "Server1" });
+    const server1Url = response.data.address;
+
+    // Attendre une demi-seconde avant d'envoyer la requête "ping" vers le serveur 1
+    setTimeout(async () => {
+      await axios.post(`${server1Url}`, { message: "ping" });
+    }, 2000);
+    
+    res.send("Request sent successfully.");
+  } else {
+    res.status(400).send("Bad Request");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server 2 is running on http://localhost:${port}`);
+});
