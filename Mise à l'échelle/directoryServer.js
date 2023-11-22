e23b72c43c357798a85ef57655735ad634f7f93f
Quang Minh PHAM <quangminh1575@gmail.com>
2023-11-22 17:44:57 +0100

    fea: Mise à l'échelle

---
+++ Mise à l'échelle/directoryServer.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 5000;
+
+// Utilisation de bodyParser pour traiter le corps des requêtes POST
+app.use(bodyParser.json());
+
+const serverAddresses = {};
+
+app.post("/register", (req, res) => {
+  const { serverName, address } = req.body;
+  if (serverName && address) {
+    serverAddresses[serverName] = address;
+    console.log(`Server ${serverName} registered with address ${address}`);
+    res.send(`Server ${serverName} registered successfully.`);
+  } else {
+    res.status(400).send("Bad Request");
+  }
+});
+
+app.post("/getAddress", (req, res) => {
+  const { serverName } = req.body;
+  if (serverName && serverAddresses[serverName]) {
+    res.send({ address: serverAddresses[serverName] });
+  } else {
+    res.status(404).send("Server not found in the directory.");
+  }
+});
+
+app.listen(port,'0.0.0.0', () => {
+  console.log(`Directory Server is running on http://0.0.0.0:${port}`);
+});
---
+++ Mise à l'échelle/server1.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+
+const directoryServerUrl = "http://app-server3:5000";
+let server2Url;
+
+// Informer le serveur annuaire de son adresse
+axios.post(`${directoryServerUrl}/register`, {
+  serverName: "Server1",
+  address: `http://app-server1:${port}`,
+});
+
+app.get("/", async (req, res) => {
+  // Obtenir l'adresse du serveur 2 depuis le serveur annuaire
+  const response = await axios.post(`${directoryServerUrl}/getAddress`, {
+    serverName: "Server2",
+  });
+  server2Url = response.data.address;
+  console.log(server2Url);
+
+  // Envoi de la requête "pong" vers le serveur 2
+  try {
+    await axios.post("http://relay-server:6000", {
+      destination: "Server2",
+      message: "pong",
+    });
+  } catch (error) {
+    console.error(
+      "Error status:",
+      error.response ? error.response.status : "N/A"
+    );
+    console.error("Error message:", error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.use(bodyParser.json());
+
+app.post("/", async (req, res) => {
+  // Vérification de la présence de la propriété 'message' dans le corps de la requête
+  if (req.body && req.body.message === "ping") {
+    console.log("ping");
+    setTimeout(async () => {
+      await axios.post(`${server2Url}`, {
+        message: "pong",
+      });
+    }, 2000);
+  } else {
+    res.status(400).send("Bad Request");
+  }
+});
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server 1 is running on http://0.0.0.0:${port}`);
+});
